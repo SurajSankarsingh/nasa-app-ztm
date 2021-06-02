fe1760e80f1270f5cac97390c771b333956c456c
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-02 16:25:31 -0400

    (feat: server - added post capability to lauches model, controller, route and modified global path in app.js. Also setup validation for missing data and valid date on server. client - updated request.js post request and repsonse for submission)

--- server/src/app.js
+++ server/src/app.js
@@ -18,8 +18,8 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
