1a00127226f82f26882c7c3685112e741506769c
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-03 14:14:03 -0400

    (fix: added build-client script and created a deploy script to run client build on server port, tested and working)

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,13 @@ app.use(
 app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 
 app.use('/planets', planetsRouter);
 app.use('/launches', launchesRouter);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
 });
 
 module.exports = app;
