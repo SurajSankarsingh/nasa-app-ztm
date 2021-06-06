3b42cef19a8565170e758be8806d9607c5c926d9
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-06 09:30:47 -0400

    (feat: added mongoDB and setup mongoose to handle connection)

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,33 @@
 const http = require('http');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://nasa-api-ztm:${process.env.MONGO}@nasaapicluster.ewlmk.mongodb.net/nasa?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
-  
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
