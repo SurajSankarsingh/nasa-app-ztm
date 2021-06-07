84db18a90f3e86aa59a2bf28949499baf9d1941d
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-07 00:21:04 -0400

    (feat: server - added ability to fetch data by limit or page using query parameters on a get request. Cleared mongo db and repopulated with live data from spacex api)

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: 'Kepler Exploration X', //name
-  rocket: 'Explorer IS1', //rocket.name
-  launchDate: new Date('December 27, 2030'), //date_local
-  target: 'Kepler-442 b', //not applicable
-  customer: ['ZTM', 'NASA'], //payloads.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
 async function populateLaunches() {
@@ -104,8 +91,12 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDB.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDB
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
