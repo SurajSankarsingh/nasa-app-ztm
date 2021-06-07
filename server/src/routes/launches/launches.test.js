4d32d5ec0504f9741651de8ff168a2ddd6731fb0
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-06-06 21:32:20 -0400

    (feat: versioned api to v1, made all changes on both server and frontend to fetch the correct data)

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -14,7 +14,7 @@ describe('Launches API', () => {
   describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => {
       const response = await request(app)
-        .get('/launches')
+        .get('/v1/launches')
         .expect('Content-Type', /json/)
         .expect(200);
     });
@@ -43,7 +43,7 @@ describe('Launches API', () => {
 
     test('It should respond with 201 created', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(data)
         .expect('Content-Type', /json/)
         .expect(201);
@@ -56,7 +56,7 @@ describe('Launches API', () => {
     });
     test('It should catch missing required properties', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(dataWithoutDate)
         .expect('Content-Type', /json/)
         .expect(400);
@@ -67,7 +67,7 @@ describe('Launches API', () => {
     });
     test('It should catch invalid dates', async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(dataWithInvalidDate)
         .expect('Content-Type', /json/)
         .expect(400);
